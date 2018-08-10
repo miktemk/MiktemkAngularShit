c9dc40f74903b3921512cf5cce0b756c4cc07795
Mikhail Temkine <miktemk@gmail.com>
2018-08-10 12:57:18 -0400

    shooting bullets via Phaser.Group

--- src/app/test-phaser-page/test-phaser-page.component.ts
+++ src/app/test-phaser-page/test-phaser-page.component.ts
@@ -18,15 +18,22 @@ export class TestPhaserPageComponent implements OnInit {
 
 }
 
+// TODO: move to commons so this is available GLOBALLY
+const nameof = <T>(name: keyof T) => name;
+
 class MyGameStateMain extends Phaser.State {
   bgScrolling: Phaser.TileSprite;
   player: Phaser.Sprite;
+  bullets: Phaser.Group;
   cursors: Phaser.CursorKeys;
-
+  fireButton: Phaser.Key;
+  bulletTime: number;
+  
   preload(game: Phaser.Game) {
     game.load.image('bg', 'assets/img/test-anime.jpg');
     game.load.image('girl', 'assets/img/duke3d-stripper.gif');
-    game.load.image('player', 'assets/img/svg-test.svg');
+    game.load.image('player', 'assets/img/player.svg');
+    game.load.image('bullet', 'assets/img/bullet.svg');
   }
 
   create(game: Phaser.Game) {
@@ -38,12 +45,23 @@ class MyGameStateMain extends Phaser.State {
 
     this.cursors = game.input.keyboard.createCursorKeys();
 
+    this.bullets = game.add.group();
+    this.bullets.enableBody = true;
+    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    this.bullets.createMultiple(30, 'bullet');
+    this.bullets.setAll('anchor.x', 0.5);
+    this.bullets.setAll('anchor.y', 1);
+    this.bullets.setAll(nameof<Phaser.Sprite>('outOfBoundsKill'), true);
+    this.bullets.setAll(nameof<Phaser.Sprite>('checkWorldBounds'), true);
+
+    this.fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    this.bulletTime = game.time.now + 200;
+    
     //var girl = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'girl');
     //girl.anchor.setTo(1, 1);
     //girl.anchor.setTo(0, 0);
     //girl.anchor.setTo(0.5, 0.5);
-
-    
   }
 
   update(game: Phaser.Game) {
@@ -53,6 +71,19 @@ class MyGameStateMain extends Phaser.State {
       this.player.body.velocity.x = -350;
     if (this.cursors.right.isDown)
       this.player.body.velocity.x = 350;
+    if (this.fireButton.isDown)
+      this.fireBullets(game);
+  }
+
+  fireBullets(game: Phaser.Game) {
+    if (game.time.now > this.bulletTime) {
+      var bullet = this.bullets.getFirstExists(false);
+      if (bullet) {
+        bullet.reset(this.player.x, this.player.y);
+        bullet.body.velocity.y = -300;
+        this.bulletTime = game.time.now + 200;
+      }
+    }
   }
 }
 
