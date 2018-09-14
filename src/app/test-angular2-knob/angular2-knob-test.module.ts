5f6f7c05f5067d488af713ed0c7a2770d278c94c
Mikhail Temkine <miktemk@gmail.com>
2018-09-14 13:58:09 -0400

    angular2-knob: fix did not work

--- src/app/test-angular2-knob/angular2-knob-test.module.ts
+++ src/app/test-angular2-knob/angular2-knob-test.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import { KnobModule } from 'angular2-knob'; //TODO: 817e6c2b: had to disable since upgrade to 6
+import { KnobModule } from 'angular2-knob'; //TODO: 817e6c2b: had to disable since upgrade to 6
 
 import { Angular2KnobTestComponent } from './angular2-knob-test.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    //KnobModule, //TODO: 817e6c2b: had to disable since upgrade to 6
+    KnobModule, //TODO: 817e6c2b: had to disable since upgrade to 6
   ],
   exports: [
     Angular2KnobTestComponent
